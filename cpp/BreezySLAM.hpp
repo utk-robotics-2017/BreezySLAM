8f3a279a68da809331f4837cdfde2f3437c21178
Andrew Messing <amessing@utk.edu>
2018-05-01 15:36:02 -0400

    centralized

--- cpp/BreezySLAM.hpp
+++ cpp/BreezySLAM.hpp
@@ -18,3 +18,7 @@
 * You should have received a copy of the GNU Lesser General Public License 
 * along with this code.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+#include "Velocities.hpp"
+#include "Position.hpp"
+#include "algorithms.hpp"
